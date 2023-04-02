7e1029416f8f4140ad2acd80de2875a865f60001
Stefan Allchorne <allchorne39@sky.com>
2023-04-02 15:19:18 +0100

    Format date using moment instead of luxon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const { DateTime } = require("luxon") // For date formatting
+const moment = require("moment") // For date formatting
 const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime") //For blog post read time
 const htmlmin = require("html-minifier");
 
@@ -8,8 +8,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/style/")
 
     // From 11tyrocks: https://11ty.rocks/eleventyjs/dates/
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    eleventyConfig.addFilter("prettyDate", (time) => {
+        return moment(time).format('MMMM Do YYYY');
     })
 
     // From https://www.npmjs.com/package/@11tyrocks/eleventy-plugin-emoji-readtime
@@ -21,28 +21,10 @@ module.exports = function(eleventyConfig) {
         bucketSize: 1,
     })
 
-    // For adding categories
-    // From: https://lewisdale.dev/post/adding-categories-to-eleventy/
-    eleventyConfig.addCollection('categories', (collectionApi) => {
-        const posts = collectionApi
-            .getFilteredByTag("posts")
-            .filter(p => !p.data.tags.includes("draft"));
-
-        return posts.reduce((tags, post) => {
-            post.data.tags.filter(tag => tag !== 'posts').forEach(tag => {
-                if (!tags[tag]) {
-                    tags[tag] = 0;
-                }
-                tags[tag]++;
-            });
-            return tags;
-        }, {"All posts": posts.length})
-    });
-
     // Automatically open up the browser on script runs
-    eleventyConfig.setBrowserSyncConfig({
-        open: true
-    })
+    // eleventyConfig.setBrowserSyncConfig({
+    //     open: true
+    // })
 
     eleventyConfig.addTransform ('htmlmin', content => {
         if (process.env.NODE_ENV === 'production') {
